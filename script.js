26e1697b12a51245b2602e97444b8aae42a37b8d
X474X <133019572+X474X@users.noreply.github.com>
2023-08-29 00:42:35 +0300

    Finished basic structure.

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const hats = document.querySelector(".cap");
 const hatsImages = ["Images/Head/fedora-1.png", "Images/Head/fedora-2.png"];
 let hatsIndex = 0;
 let eyeIndex = 0;
+let noseIndex = 0;
+let mouthIndex = 0;
 
 const eyeRight = document.querySelector(".eyes-arrow-right");
 const eyeLeft = document.querySelector(".eyes-arrow-left");
@@ -13,6 +15,27 @@ const eyesImages = [
   "Images/Eyes/ochi1.png",
   "Images/Eyes/ochi2.png",
   "Images/Eyes/ochi3.png",
+];
+
+const noseRight = document.querySelector(".nose-arrow-right");
+const noseLeft = document.querySelector(".nose-arrow-left");
+const nose = document.querySelector(".nose");
+const noseImages = [
+  "Images/Nose/nas3.png",
+  "Images/Nose/nas4.png",
+  "Images/Nose/nas5.png",
+  "Images/Nose/nas6.png",
+  "Images/Nose/Nas 3.png",
+];
+
+const mouthRight = document.querySelector("mouth-arrow-right");
+const motuhLeft = document.querySelector(".mouth-arrow-left");
+const mouth = document.querySelector(".mouth");
+const mouthImages = [
+  "Images/Mouth/SMILE.png",
+  "Images/Mouth/Gura 1.png",
+  "Images/Mouth/Gura 2.png",
+  "Images/Mouth/Gura 3.png",
 ];
 
 headLeft.addEventListener("click", () => {
@@ -39,5 +62,35 @@ eyeRight.addEventListener("click", () => {
   if (eyeIndex <= 2) {
     eyeIndex++;
     eyes.src = eyesImages[eyeIndex];
+  }
+});
+
+noseRight.addEventListener("click", () => {
+  if (noseIndex <= 3) {
+    noseIndex++;
+    nose.src = noseImages[noseIndex];
+    console.log(noseIndex);
+  }
+});
+
+noseLeft.addEventListener("click", () => {
+  if (noseIndex > 0) {
+    noseIndex--;
+    nose.src = noseImages[noseIndex];
+    console.log(noseIndex);
+  }
+});
+
+mouthRight.addEventListener("click", () => {
+  if (mouthIndex <= 3) {
+    mouthIndex++;
+    mouth.src = mouthImages[mouthIndex];
+  }
+});
+
+mouthLeft.addEventListener("click", () => {
+  if (mouthIndex < 0) {
+    mouthIndex--;
+    mouth.src = mouthImages[mouthIndex];
   }
 });
